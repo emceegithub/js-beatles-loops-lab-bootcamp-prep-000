a16f81811017c5085808c18a4a380d22f941a9c7
Mark Cruz <markchriscruz@yahoo.com>
2018-02-14 02:39:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function theBeatlesPlay(arrayOfMusicians, arrayOfInstruments) {
+  var newArray = []
+  for (var i=0;i<arrayOfMusicians.length;i++) {
+    newArray[i] = arrayOfMusicians[i] + " plays " + arrayOfInstruments[i]
+  }
+  return newArray
   
 }
